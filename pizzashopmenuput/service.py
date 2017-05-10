dc4bb46e7e7ba6ae42b723a54bfee64db0b61169
Pranav Prem <pranavprem93@gmail.com>
2017-05-09 19:36:47 -0700

    put working. expected return object obtained

--- pizzashopmenuput/service.py
+++ pizzashopmenuput/service.py
@@ -6,8 +6,16 @@ def handler(event, context):
     client = boto3.client("dynamodb")
     
     try:
-        
         client.update_item(TableName="pizzashopmenu", Key=event, UpdateExpression="SET #sel = list_append(#sel, :val1)",ExpressionAttributeNames={"#sel": "selection"}, ExpressionAttributeValues={":val1":{"L":[{"S":"Vegetable"}]}})
+        item = client.get_item(TableName="pizzashopmenu", Key=event)
+        response = {}
+        response ["menu_id"] = item["Item"]["menu_id"]["S"]
+        response ["selection"] = [select["S"] for select in item["Item"]["selection"]["L"]]
     except Exception,e:
         return 400, e
-    return 200,"OK"
+    return response, 200, "OK"
+
+""" body mapping templates: 
+ input:
+{"menu_id":{"S": "$input.params('menu-id')"}}
+"""
