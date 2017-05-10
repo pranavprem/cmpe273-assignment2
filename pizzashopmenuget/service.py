353234c8bc06a9d20067781de41d4fae4ebae3db
Pranav Prem <pranavprem93@gmail.com>
2017-05-09 18:13:17 -0700

    added body mapping templates for input and output

--- pizzashopmenuget/service.py
+++ pizzashopmenuget/service.py
@@ -13,6 +13,31 @@ def handler(event, context):
     store_hours["Fri"] = response["Item"]["store_hours"]["M"]["Fri"]["S"]
     store_hours["Sat"] = response["Item"]["store_hours"]["M"]["Sat"]["S"]
     store_hours["Sun"] = response["Item"]["store_hours"]["M"]["Sun"]["S"]
-    # body mapping template: {"S": "$input.params('menu-id')"}
     response["Item"]["store_hours"] = store_hours
     return response
+
+
+
+""" body mapping templates: 
+ input:
+{"menu_id":{"S": "$input.params('menu-id')"}}
+
+output:
+#set($inputRoot = $input.path('$'))
+{
+    "menu_id": "$inputRoot.Item.menu_id.S",
+    "store_name": "$inputRoot.Item.store_name.S",
+    "selection": [
+		#foreach($elem in $inputRoot.Item.selection.L)"$elem.S"#if($foreach.hasNext),#end#end
+    ],
+    "size": [
+	    #foreach($elem in $inputRoot.Item.size.L)"$elem.S"#if($foreach.hasNext),#end#end
+	    ],
+    "price": [
+	    #foreach($elem in $inputRoot.Item.price.L)"$elem.N"#if($foreach.hasNext),#end#end
+    ],
+    "store_hours":{
+    	$inputRoot.Item.store_hours
+    }
+}
+"""
